8239685be3acf7c00aec9042a8b54b3287c164bd
RomanRVV <rvachyov.roma@yandex.ru>
2023-08-08 18:00:08 +0300

    add redirect check function

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ import requests
 from pathlib import Path
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
+
 Path("books").mkdir(parents=True, exist_ok=True)
 
 number_of_books = 10
 book_id = 0
 for a in range(number_of_books):
     book_id += 1
-    url = f"https://tululu.org/txt.php?id={book_id}"
+    payload = {
+        'id': book_id
+    }
+    url = "https://tululu.org/txt.php"
 
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
+    try:
+        check_for_redirect(response)
+        with open(f"books/id{book_id}.txt", 'wb') as file:
+            file.write(response.content)
+    except requests.HTTPError:
+        print(f'Книги с id {book_id}, нет')
 
-    with open(f"books/id{book_id}.txt", 'wb') as file:
-        file.write(response.content)
